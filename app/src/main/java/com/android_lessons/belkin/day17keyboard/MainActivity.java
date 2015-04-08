92abb2fda23361967ac4650edf6566efda30540f
Belka <Evgenia_@email.ua>
2015-04-08 13:53:56 +0300

    listener for device buttons

--- app/src/main/java/com/android_lessons/belkin/day17keyboard/MainActivity.java
+++ app/src/main/java/com/android_lessons/belkin/day17keyboard/MainActivity.java
@@ -1,12 +1,19 @@
 package com.android_lessons.belkin.day17keyboard;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebChromeClient;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private static long back_pressed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +21,65 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
+    @Override
+    public void onBackPressed() {
+        //super.onBackPressed();
+        openQuitDialog();
+
+        /*if(back_pressed + 200 > System.currentTimeMillis())
+            super.onBackPressed();
+        else
+            Toast.makeText(getBaseContext(), "Press once again to exit!", Toast.LENGTH_SHORT).show();
+        back_pressed = System.currentTimeMillis();*/
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // TODO Auto-generated method stub
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_MENU:
+                Toast.makeText(this, "Нажата кнопка Меню", Toast.LENGTH_SHORT)
+                        .show();
+                return true;
+            case KeyEvent.KEYCODE_SEARCH:
+                Toast.makeText(this, "Нажата кнопка Поиск", Toast.LENGTH_SHORT)
+                        .show();
+                return true;
+            case KeyEvent.KEYCODE_BACK:
+                onBackPressed();
+                return true;
+            case KeyEvent.KEYCODE_VOLUME_UP:
+                event.startTracking();
+                return true;
+            case KeyEvent.KEYCODE_VOLUME_DOWN:
+                Toast.makeText(this, "Нажата кнопка громкости", Toast.LENGTH_SHORT)
+                        .show();
+                return false;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    private void openQuitDialog(){
+        AlertDialog.Builder quitDialog = new AlertDialog.Builder(MainActivity.this);
+        quitDialog.setTitle("Выход: Вы уверены?");
+
+        quitDialog.setPositiveButton("Таки да!", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+
+        quitDialog.setNegativeButton("Нет", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+        quitDialog.show();
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
